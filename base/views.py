214b960dd80695f17b2b0d437e4cbdac38ad7cbc
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-07-15 11:55:38 +0300

    Add to cart functionality

--- base/views.py
+++ base/views.py
@@ -3,8 +3,12 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .products import products
+from .models import Product
+from .serializer import ProductSerializer
 
 # Create your views here.
+
+
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
@@ -18,21 +22,23 @@ def getRoutes(request):
         'api/products/<update>/<id>/',
     ]
     return Response(routes)
+
+
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
-def getProduct(request,pk):
-    product = None
-    
+def getProduct(request, pk):
+
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+
     for i in products:
         if i['_id'] == pk:
             product = i
             break
-    return Response(product)
-    
-
-    
-    
+    return Response(serializer.data)
