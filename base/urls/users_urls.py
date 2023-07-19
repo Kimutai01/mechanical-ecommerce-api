8c469c8a849c0df3f574f31db0ead751f1d27842
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-07-19 23:37:14 +0300

    User update backend route

--- base/urls/users_urls.py
+++ base/urls/users_urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path('', views.getUsers, name="users"),
     path('register/', views.registerUser, name="register"),
     path('profile/update/', views.updateUserProfile, name="user-profile-update"),
+    path('update/<str:pk>/', views.updateUser, name="user-update"),
+    path('<str:pk>/', views.getUserById, name="user"),
     path('delete/<str:pk>/', views.deleteUser, name="user-delete"),
+
 
 ]
