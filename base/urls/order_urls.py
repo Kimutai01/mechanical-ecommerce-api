b474252d6ee8606cbb0faa94537693648e35ade3
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-07-19 21:52:08 +0300

    Added Url for myorders

--- base/urls/order_urls.py
+++ base/urls/order_urls.py
@@ -4,6 +4,7 @@ from base.views import order_views
 
 urlpatterns = [
     path('add/', order_views.addOrderItems, name='orders-add'),
+    path('myorders/', order_views.getMyOrders, name='myorders'),
     path('<str:pk>/', order_views.getOrderById, name='user-order'),
     path('<str:pk>/pay/', order_views.updateOrderToPaid, name='pay'),
 
